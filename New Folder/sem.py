e23ad57e87a7160132e8c7340cfdb232c2d4e1fb
violetta31 <violett.kiseleva@yandex.ru>
2022-02-08 13:10:15 +0300

    4

--- New Folder/sem.py
+++ New Folder/sem.py
@@ -1,7 +1,7 @@
 Height = 168
 Weight = 59
-Steps = 5000
-Time = 120
+Steps = 4000
+Time = 160
 Rash = 0.035 * Weight + ((Steps/Time)**2 / Height) * 0.029 * Weight
-Distance = Steps*(Height/4+0.37)
+Distance = Steps*(Height/4+0.37)/1000
 print(f"Калорий сожжено: {Rash}; Пройденная дистанция: {Distance}")
