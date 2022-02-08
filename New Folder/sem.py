b524ea7ebc48b05e80c2aecfbe44dcc128e05be5
violetta31 <violett.kiseleva@yandex.ru>
2022-02-08 13:22:48 +0300

    5

--- New Folder/sem.py
+++ New Folder/sem.py
@@ -2,6 +2,12 @@ Height = 168
 Weight = 59
 Steps = 4000
 Time = 160
-Rash = 0.035 * Weight + ((Steps/Time)**2 / Height) * 0.029 * Weight
-Distance = Steps*(Height/4+0.37)/1000
-print(f"Калорий сожжено: {Rash}; Пройденная дистанция: {Distance}")
+callories = 0.035 * Weight + ((Steps/Time)**2 / Height) * 0.029 * Weight
+distance = Steps*(Height/4+0.37)/1000
+print(f"Калорий сожжено: {callories}; Пройденная дистанция: {distance}")
+if (distance < 2):
+    print("Ходите больше!")
+elif (distance > 2 and distance < 4):
+    print("Ещё чуть-чуть!")
+else :
+    print("Молодец!")
